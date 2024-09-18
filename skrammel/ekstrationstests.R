38acfe8024442161cd09891bc00f4f341fc850c0
Christian B. Knudsen <cbk@kb.dk>
2024-09-18 13:56:41 +0200

    Et skridt videre i ekstraktion af placemarks.

--- skrammel/ekstrationstests.R
+++ skrammel/ekstrationstests.R
@@ -15,10 +15,41 @@ test
 xml_data <- test
 
 
+
+
+
+xml_data <- xml_data %>%
+  mutate(startIndex = (xml_data %>% filter(navn == "startIndex") %>% pull(indhold) %>% .[[1]] %>% xml_text()),
+                    itemsPerPage = (xml_data %>% filter(navn == "itemsPerPage") %>% pull(indhold) %>% .[[1]] %>% xml_text()),
+                    totalResults = (xml_data %>% filter(navn == "totalResults") %>% pull(indhold) %>% .[[1]] %>% xml_text())
+) %>%
+  filter(navn == "Placemark") %>%
+  mutate(placemark = map(indhold, function(x) tibble(navn = x %>% xml_children() %>% xml_name(),
+                                                     indhold = x %>% xml_children() %>% lapply(function(x) x))))  %>%
+  select(-c(navn, indhold))
+
+xml_data <- xml_data %>% mutate(ID = 1:n()) %>% unnest(placemark) %>%
+
+  pivot_wider(names_from = navn, values_from = indhold) %>%
+  mutate(across(name:Point,  ~map(.x, html_text))) %>%
+  mutate(across(name:Point,  unlist))
+
+
+xml_data %>%
+  mutate(ting = map(ExtendedData, extraher)) %>%
+ slice(1) %>%
+  pull(ting) %>%
+  .[[1]] %>%
+  slice(1) %>%
+  pull(indhold)
+
+
+
 # funktion til at ekstrahere placemarkdata.
 extraher <- function(xml_data){
 
   navne <- xml_data %>%
+    xml_child() %>%
     xml_children() %>% xml_name()
 
   indhold <- xml_data %>%
@@ -27,24 +58,3 @@ extraher <- function(xml_data){
   tibble(navn = navne,
          indhold = indhold)
 }
-
-# vi ser lige at den virker.
-xml_data %>%
-  filter(navn == "Placemark") %>%
-  transmute(placemark = map(indhold, extraher)) %>%
-  mutate(startIndex = )
-
-
-
-startIndex <- xml_data %>% filter(navn == "startIndex") %>%
-  pull(indhold) %>% .[[1]] %>%
-  xml_text()
-
-itemsPerPage <- xml_data%>% filter(navn == "itemsPerPage") %>%
-  pull(indhold) %>% .[[1]] %>%
-  xml_text()
-
-totalResults <- xml_data%>% filter(navn == "totalResults") %>%
-  pull(indhold) %>% .[[1]] %>%
-  xml_text()
-
