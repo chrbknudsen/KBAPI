1c8607189847c22d9f55cf9339a181d425cfb65b
Christian B. Knudsen <cbk@kb.dk>
2024-09-19 13:10:45 +0200

    Initial commit

---
+++ R/get_authors.R
@@ -0,0 +1,15 @@
+#' Søg forfattere fra textcollections
+#'
+#' Hjælpe funktion til at returnere forfattere fra text collections
+#'
+#' @return A tibble containing SVARET!
+#' @examples
+#' # Eksempel på brug af funktionen
+#' get_text(q = "helsinge")
+#' @export
+#'
+#'
+
+get_authors <- function(){
+
+}
