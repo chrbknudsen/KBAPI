ccbc9b043c425cf3e84260f8da15c19d739f4c9f
Christian B. Knudsen <cbk@kb.dk>
2024-09-19 13:10:56 +0200

    initial commit

---
+++ R/get_categories.R
@@ -0,0 +1,15 @@
+#' Categories i text collections
+#'
+#' returnerer kategorier fra text collections
+#'
+#' @return A tibble containing SVARET!
+#' @examples
+#' # Eksempel på brug af funktionen
+#' get_text(q = "helsinge")
+#' @export
+#'
+#'
+
+get_categories <- function(){
+
+}
